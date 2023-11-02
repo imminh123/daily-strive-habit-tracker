bd370a117ed81b8893f86bc808b9a5b54210a24d
Hyeju Lee <hyeju.lee@if.fi>
2023-11-02 17:53:29 +0200

    task properties updated

--- packages/server/src/db/models/task.model.ts
+++ packages/server/src/db/models/task.model.ts
@@ -3,25 +3,15 @@ const { Schema } = mongoose;
 
 export interface ITask {
   name: string;
-  currentProgress: Number;
-  maxProgress: Number;
   description: string;
-  notificationToggle: boolean;
-  notificationTime: Date;
   topic: ObjectId;
-  completed: boolean;
 }
 
 const TaskSchema = new Schema<ITask>(
   {
     name: String,
-    currentProgress: Number,
-    maxProgress: Number,
     description: String,
-    notificationToggle: Boolean,
-    notificationTime: Date,
     topic: { type: Schema.Types.ObjectId, ref: "Topic" },
-    completed: Boolean,
   },
   {
     timestamps: true,
