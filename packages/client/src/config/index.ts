bfb0de675a710231674b5ec28c565fb557010e30
Minh Nguyen <minhco12@gmail.com>
2023-12-02 20:45:52 +0200

    update

--- packages/client/src/config/index.ts
+++ packages/client/src/config/index.ts
@@ -1,3 +1,3 @@
 export const APP_CONFIG = {
-    API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8080/api/v1"
+    API_BASE_URL: process.env.API_BASE_URL || "https://shark-app-q65od.ondigitalocean.app/api/v1"
 }
